34c64ee6d1b0aa3acf6bc4e2d9234e02ab04beb4
Matthew Scharley <matt.scharley@gmail.com>
2016-05-23 07:52:35 +1000

    Cleanup imports

--- src/main/scala/demo/Window.scala
+++ src/main/scala/demo/Window.scala
@@ -3,27 +3,28 @@ package demo
 import nodejs.Require
 import electron._
 import autowire._
+import org.scalajs.jquery.JQueryStatic
 
 import scala.scalajs.js
-import scala.scalajs.js.Dynamic.{global => JSGlobal}
+import scala.scalajs.js.Dynamic.global
 import scala.scalajs.js.annotation.JSExport
 import scala.concurrent.Future
 
 @JSExport("Demo.Window")
-class Window(val require: Require = JSGlobal.require.asInstanceOf[Require])
+class Window(val require: Require = global.require.asInstanceOf[Require])
   extends RendererProcess(require)
 {
   import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
-  val jQuery = require("jquery").asInstanceOf[org.scalajs.jquery.JQueryStatic]
+  val jQuery = require("jquery").asInstanceOf[JQueryStatic]
 
   val api: ApiClient = new ApiClient(electron.ipcRenderer)
   val pong: Future[String] = api[Api].ping().call()
 
   pong foreach { v =>
-    JSGlobal.console.log(v)
+    global.console.log(v)
   }
 
   pong recover {
-    case e => JSGlobal.console.error(s"${e.getClass.getName}: ${e.getMessage}")
+    case e => global.console.error(s"${e.getClass.getName}: ${e.getMessage}")
   }
 }
